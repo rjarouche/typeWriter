e483b601007654244c9aa7148facad16365a93c0
Rodrigo Jarouche <rjarouche@gmail.com>
2016-09-30 02:34:31 -0300

    Update typeWriter.js

--- typeWriter.js
+++ typeWriter.js
@@ -1,3 +1,11 @@
+    /*
+      TypeWritter effect Javascript 
+      By Rodrigo Jarouche
+      http://rodrigojarouche.blogspot.com
+      Based on http://pt.stackoverflow.com/users/129/sergio
+      http://pt.stackoverflow.com/questions/64021/texto-que-se-digita-aos-poucos/155874#155874
+    */
+
     function typeWriter(texto,idElemento,tempo){
         var char = texto.split('').reverse();
         var typer = setInterval(function () {
